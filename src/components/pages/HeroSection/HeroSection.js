25b432f65c628bfe94993e4aa693330682568c44
janaina <janainaborgesdr@gmail.com>
2021-11-30 16:41:45 -0300

    build

--- src/components/pages/HeroSection/HeroSection.js
+++ src/components/pages/HeroSection/HeroSection.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from "../Button/Button";
 import "./HeroSection.css";
 import { Link } from "react-router-dom";
-import Contact from "../Contact/Contact"
 
 function HeroSection() {
     return (
